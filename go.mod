d77138837f4811bd492109bce496d2d4c4628440
raqazhet <qazhetrazaq@gmail.com>
2023-08-06 19:57:46 +0600

    error in the create

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/caarlos0/env v3.5.0+incompatible
+	github.com/gin-gonic/gin v1.9.1
 	github.com/go-playground/validator/v10 v10.14.1
 	github.com/joho/godotenv v1.5.1
 	go.mongodb.org/mongo-driver v1.12.1
@@ -15,7 +16,6 @@ require (
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.9.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
--- transport/http/handler/todo.go
+++ transport/http/handler/todo.go
@@ -2,10 +2,32 @@ package handler
 
 import (
 	"net/http"
+	"time"
+
+	"tzregion/model"
 
 	"github.com/gin-gonic/gin"
 )
 
+type todo struct {
+	title    string
+	activeAt time.Time
+}
+
 func (h *Handler) CreateTodo(ctx *gin.Context) {
+	var todo todo
+	if err := ctx.BindJSON(&todo); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid request body"})
+		return
+	}
+	todoModel := model.Todo{
+		Title:    todo.title,
+		ActiveAt: todo.activeAt,
+		Status:   "active",
+	}
+	if err := h.service.Todo.CreateTodo(ctx, &todoModel); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create todo"})
+		return
+	}
 	ctx.Status(http.StatusNoContent)
 }
